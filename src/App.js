9ef00d72ed127e60c12fc59dc40af16f82884879
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-07-08 11:37:26 -0700

    app.js work / routing fixes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import Header from './components/Header'
-import Wrapper from "./components/Wrapper";
+import Header from './components/header/header'
+// import Wrapper from "./components/Wrapper";
 import CharacterCard from './components/charCard'
 import './App.css'
 import spongebobCharacters from './characters.json'
@@ -13,43 +13,51 @@ class App extends Component {
   };
 
 
-  counterCheck=(name,selectedState)=>{
-    let charactersArray = this.state.spongebobCharacters;
-    charactersArray.sort(function(a, b){return 0.5 - Math.random()});
+      counterCheck=(name,selectedState)=>{
+        let charactersArray = this.state.spongebobCharacters;
+        charactersArray.sort(function(a, b){return 0.5 - Math.random()});
 
-    if (selectedState){
-        charactersArray.forEach(character=> character.selected = false);
-        this.setState({spongebobCharacters: charactersArray, counter: 0})
-    } else {
-        charactersArray.forEach((character) => {
-            if (character.name === name && character.selected === false) {
-                character.selected = true;
-                this.setState({spongebobCharacters: charactersArray, counter: this.state.counter + 1})
-            }
-        });
-    }
-};
+        if (selectedState){
+            charactersArray.forEach(character=> character.selected = false);
+            this.setState({spongebobCharacters: charactersArray, counter: 0})
+        } else {
+            charactersArray.forEach((character) => {
+                if (character.name === name && character.selected === false) {
+                    character.selected = true;
+                    this.setState({spongebobCharacters: charactersArray, counter: this.state.counter + 1})
+                }
+            });
+        }
+      };
 
 
-render(){
-  return(
-      <Wrapper>
-          <Header score={this.state.counter} />
-          <div className={"container"}>
-              <div className={"row"}>
-                  {this.state.fighters.map((fighter) => <CharacterCard  key={fighter.key} id ={fighter.key} character={fighter.name} selected={fighter.selected} counterCheck={this.counterCheck}/>)}
-              </div>
-          </div>
-      </Wrapper>
-  )
-};
+    render(){
+      return(
+        <wrapper>
+            <Header score={this.state.counter} />
+            <div className={"container"}>
+                <div className={"row"}>
+                    {this.state.fighters.map((character) => 
+                    <CharacterCard  
+                    key={character.key} 
+                    id ={character.key} 
+                    character={character.name} 
+                    selected={character.selected} 
+                    counterCheck={this.counterCheck}/>)}
+                </div>
+            </div>
+        </wrapper>
+      )
+      };
 }
 
 
+export default App;
 
 
 
 
 
 
-}
+
+
