21020b06e50e6ca828860724a2e1272ec5afab1f
Ramon Vidallon <ramon.vidallon@icloud.com>
2019-07-09 20:19:44 -0700

    Updates

---
+++ precache-manifest.f70aeb9edd9407328117abbbc479b062.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06271f2f0be1cac716cfc5b3f48738d0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "477474f5bc6fdf5309b3",
+    "url": "/clicky-game/static/css/main.eca498d2.chunk.css"
+  },
+  {
+    "revision": "8837555f4b8274343b5d",
+    "url": "/clicky-game/static/js/2.c828012f.chunk.js"
+  },
+  {
+    "revision": "477474f5bc6fdf5309b3",
+    "url": "/clicky-game/static/js/main.d3bcf5e7.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
